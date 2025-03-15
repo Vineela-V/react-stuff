571ab1eab05a2788892c48cdbfd8d06575cdf1fb
vineela <vineelav2000@gmail.com>
2025-03-15 12:22:05 +0530

    commit 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,12 @@
 import "./App.css";
-import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  
-  const increase = () => {
-    setCount(count + 1);
-  }
-  const decrease = () => {
-    setCount(count - 1);
-  }
-  const setToZero = () => {
-    setCount(0);
-  }
+const App = () => {
   return (
     <div className="App">
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease }>Decrease</button>
-      <button onClick={setToZero}>Set to Zero </button>
-
-      {count}
+      <button> Generate Cat Fact </button>
+      <p> </p>
     </div>
-  );
+  )
 }
 
 export default App
-
